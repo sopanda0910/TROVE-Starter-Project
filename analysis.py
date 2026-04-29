d5ea7904ce2648a2593e1aba0a6d4a3a36c19677
sopanda0910 <63028301+sopanda0910@users.noreply.github.com>
2026-04-29 13:36:30 +0100

    Removed Unfiltered Correlations

--- analysis.py
+++ analysis.py
@@ -10,10 +10,7 @@ full_data = full_data.dropna(axis=0)
 threshold = 1e-2
 data_clean = full_data[full_data['dist'] > threshold]
 relevant_data = data_clean[['2D', 'dist', 'phot_KN', 'phot_KN-in-SN', 'phot_super-KN', 'predet_KN', 'predet_KN-in-SN', 'predet_super-KN']]
-relevant_data_unfiltered = full_data[['2D', 'dist', 'phot_KN', 'phot_KN-in-SN', 'phot_super-KN', 'predet_KN', 'predet_KN-in-SN', 'predet_super-KN']]
 
 corr_matrix = relevant_data.corr(method='spearman')
-corr_matrix_unfiltered = relevant_data_unfiltered.corr(method='spearman')
 
 corr_matrix.to_csv('out/corrs/filtered_correlations.csv')
-corr_matrix_unfiltered.to_csv('out/corrs/unfiltered_correlations.csv')
